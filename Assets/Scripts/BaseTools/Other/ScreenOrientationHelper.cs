976ab8b3b7e19644f02cde6cf8ed1d30f55799a0
zheng.cui <zheng.cui@happyelements.com>
2020-05-13 13:00:09 +0800

    1

---
+++ Assets/Scripts/BaseTools/Other/ScreenOrientationHelper.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+
+public static class ScreenOrientationHelper
+{
+
+    public enum NScreenOrientation
+    {
+        LandscapeLeft,
+        LandscapeRight,
+        Portrait,
+        PortraitUpsideDown,
+        AutoRotation,
+        LandspaceAutoRotation,//横屏两方向切换
+        PortraitAutoRotation//竖屏两方向切换
+    }
+
+    static void Start()
+    {
+        Screen.orientation = ScreenOrientation.AutoRotation;
+        Screen.autorotateToLandscapeLeft = true;
+        Screen.autorotateToLandscapeRight = true;
+        Screen.autorotateToPortrait = false;
+        Screen.autorotateToPortraitUpsideDown = false;
+    }
+
+    public static void SetOrientation(NScreenOrientation type) 
+    {
+        switch (type) 
+        {
+            case NScreenOrientation.LandscapeLeft:
+                Screen.orientation = ScreenOrientation.LandscapeLeft;
+                break;
+            case NScreenOrientation.LandscapeRight:
+                Screen.orientation = ScreenOrientation.LandscapeRight;
+                break;
+            case NScreenOrientation.Portrait:
+                Screen.orientation = ScreenOrientation.Portrait;
+                break;
+            case NScreenOrientation.PortraitUpsideDown:
+                Screen.orientation = ScreenOrientation.PortraitUpsideDown;
+                break;
+            case NScreenOrientation.AutoRotation:
+                Screen.orientation = ScreenOrientation.AutoRotation;
+                Screen.autorotateToLandscapeLeft = true;
+                Screen.autorotateToLandscapeRight = true;
+                Screen.autorotateToPortrait = true;
+                Screen.autorotateToPortraitUpsideDown = true;
+                break;
+            case NScreenOrientation.LandspaceAutoRotation:
+                Screen.orientation = ScreenOrientation.AutoRotation;
+                Screen.autorotateToLandscapeLeft = true;
+                Screen.autorotateToLandscapeRight = true;
+                Screen.autorotateToPortrait = false;
+                Screen.autorotateToPortraitUpsideDown = false;
+                break;
+            case NScreenOrientation.PortraitAutoRotation:
+                Screen.orientation = ScreenOrientation.AutoRotation;
+                Screen.autorotateToLandscapeLeft = false;
+                Screen.autorotateToLandscapeRight = false;
+                Screen.autorotateToPortrait = true;
+                Screen.autorotateToPortraitUpsideDown = true;
+                break;
+        }  
+
+    }
+
+
+
+}
---
+++ Assets/Scripts/Game/StageRoot.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class StageRoot : MonoBehaviour
+{
+
+    [SerializeField] Button BackBtn;
+    private void Awake()
+    {
+        ScreenOrientationHelper.SetOrientation(ScreenOrientationHelper.NScreenOrientation.LandspaceAutoRotation);
+    }
+
+    public void TempClick() 
+    {
+        SceneManager.LoadScene("Main");
+    }
+
+}
