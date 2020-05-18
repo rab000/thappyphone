514bdcdcc963bace9c6b9b693ffe490b462ea799
zheng.cui <zheng.cui@happyelements.com>
2020-05-18 17:10:32 +0800

    1

---
+++ Assets/Scripts/GamePlay/UI/AR/ARPanel.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class ARPanel : MonoBehaviour
+{
+    [SerializeField] Button ExitBtn;
+
+
+    private void OnEnable()
+    {
+        ExitBtn.onClick.AddListener(OnClickExit);
+    }
+
+    private void OnDisable()
+    {
+        ExitBtn.onClick.RemoveListener(OnClickExit);
+    }
+
+    private void OnClickExit() 
+    {
+        AppMgr.GetIns().SwitchScn("MainMenu");
+    }
+   
+
+}
