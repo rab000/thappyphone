54021608c30919429ffabee94b133689d84f546c
zheng.cui <zheng.cui@happyelements.com>
2020-05-18 17:49:48 +0800

    1

---
+++ Assets/Scripts/GamePlay/UI/loadingWindow.cs
@@ -0,0 +1,128 @@
+using UnityEngine.UI;
+using UnityEngine;
+
+public class loadingWindow :MonoBehaviour {
+
+	public Image BgImg;
+	public Slider LoadingSlider;
+	public Text TextInfo;
+	public Text TextProgress;
+
+    /// <summary>
+    /// 是否启动进度平滑
+    /// </summary>
+    public bool BeProgressSmoth = false;
+
+	public void Awake ()
+    {
+        DontDestroyOnLoad(gameObject);
+        //Debug.LogError("GameLoading打开时注册更新事件");
+        ntools.Messenger.AddListener<float>("updateLoadingProgress", UpdateProgress);
+        ntools.Messenger.AddListener<string>("updateLoadingText", UpdateInfoText);
+    }
+
+	public void Open(object uiDatas=null)
+    {	
+        LoadingSlider.value = 0;
+	}
+    
+    private void UpdateProgress(float progress)
+    {
+        //Debug.LogError("接收 progress:"+progress);
+
+        int intProgress = (int)(progress * 100);
+
+        if (BeProgressSmoth)
+        {
+            //这里的问题，更新进度的时候要更新文字           
+            CurRealProcess = intProgress;
+        }
+        else
+        {
+            TextProgress.text = intProgress.ToString();
+            LoadingSlider.value = progress;
+
+            if (progress == 1.0f)
+            {
+                ActionAfterLoading?.Invoke();
+            }
+        }
+    }
+
+	public void SetBgImg(Sprite sprite)
+    {
+		BgImg.overrideSprite = sprite;
+	}
+
+    private void UpdateInfoText(string str)
+    {
+        //Debug.LogError("更新loading text:" + str);
+        TextInfo.text = str;
+	}
+   
+    public  void Clear(){
+        
+		LoadingSlider.value = 0f;
+		TextInfo.text = "载入中...";
+		TextProgress.text ="载入中0";
+		CurRealProcess = 0f;
+		CurProcess = 0f;
+        ntools.Messenger.RemoveListener<float>("updateLoadingProgress", UpdateProgress);
+        ntools.Messenger.RemoveListener<string>("updateLoadingText", UpdateInfoText);
+    }
+
+    #region lecacy
+
+    /// <summary>
+	/// 当前真实加载进度,最大不超过100
+	/// </summary>
+	public float CurRealProcess;
+
+    /// <summary>
+    /// 当前实时加载进度,最大不超过100
+    /// </summary>
+    public float CurProcess;
+
+    private const float LOAD_SPEED_FACTOR = 0.01f;
+
+    private Listener ActionAfterLoading;
+
+    /// <summary>
+    /// 设置loading后续回调
+    /// </summary>
+    /// <param name="callback"></param>
+    public void SetActionAfterLoading(Listener callback)
+    {
+        ActionAfterLoading = callback;
+    }
+
+    public void Update()
+    {
+
+        if (!BeProgressSmoth) return;
+
+        if (CurProcess >= 100)
+        {           
+            ActionAfterLoading?.Invoke();
+            return;
+        }
+                
+        if (CurProcess < CurRealProcess)
+        {
+            CurProcess++;
+        }
+        else if (CurProcess > CurRealProcess)
+        {
+            CurProcess = CurRealProcess;
+        }
+
+        TextProgress.text = CurProcess.ToString();
+
+        float f = CurProcess * LOAD_SPEED_FACTOR;
+
+        LoadingSlider.value = f;
+
+    }
+    #endregion lecacy
+
+}
--- Assets/Scripts/Manager/SimpleResMgr.cs
+++ Assets/Scripts/Manager/SimpleResMgr.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SimpleResMgr : SingletonBehaviour<SimpleResMgr>
 {
@@ -12,6 +13,32 @@ public class SimpleResMgr : SingletonBehaviour<SimpleResMgr>
     public void LoadBundle() 
     {
         
+    }
+
+    IEnumerator wait()
+    {
+        string path = Application.streamingAssetsPath + "/test.ab";//ab包位置//test.ab是我ab包的名字
+        WWW www = new WWW(path);
+        yield return www;
+        if (string.IsNullOrEmpty(www.error))
+        {
+            //获取到ab包资源
+            AssetBundle bundle = www.assetBundle;
+            //输出所有资源地址
+            string[] strs = bundle.GetAllScenePaths();
+            foreach (string str in strs) Debug.Log(str);
+            //一步读取场景
+            AsyncOperation async = SceneManager.LoadSceneAsync("Test");//不需要带后缀//这里Test是我的场景名字
+            async.allowSceneActivation = false;
+            while (async.progress < 0.9f)
+            {
+                Debug.Log("场景进度  " + async.progress);
+                yield return null;
+            }
+            async.allowSceneActivation = true;
+            yield break;
+        }
+        Debug.Log("读取错误  " + www.error);
     }
 
 }
